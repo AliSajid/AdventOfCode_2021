dcd81470dba132c3496c60449002aa0a8a7ed8dd
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2021-12-12 09:45:33 -0500

    Added annotations in the script

--- day_1.R
+++ day_1.R
@@ -3,16 +3,22 @@
 library(tidyverse)
 library(zoo)
 
+#-------------- Set up the Problem
+
 input_data_file <- "input/input_1"
 
 data <- read_tsv(input_data_file, col_names = c("depth")) |>
     pull(depth)
+
+#-------------- Solve Part A
 
 larger <- sum(
     data < data[2:2000]
 )
 
 larger
+
+#-------------- Solve Part B
 
 windowed <- rollsum(data, 3)
 
